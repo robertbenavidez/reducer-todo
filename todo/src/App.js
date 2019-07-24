d1da688f86581fed1da7e3a4acc65683db9739b7
Robert Benavidez <murabitbenavidez@gmail.com>
2019-07-24 05:32:56 -0400

    built a reducer and set up intial state

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+
+import { reducer, initialState } from './reducers/reducer';
 
 import './App.css';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
   return (
     <div className="App">
       <h1>Reducer Todo App</h1>
